81ae8228b16ca6b78bb9a4ccb486ee317fd4db66
ccsd9868 <905736705@qq.com>
2018-08-22 16:30:13 +0800

    初始化项目

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by cykj on 2018/8/22.
+//  Copyright © 2018年 cykj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo
+//
+//  Created by cykj on 2018/8/22.
+//  Copyright © 2018年 cykj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
